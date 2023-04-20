030c54ebd0dc7350e976e3d84ef512694efdba83
Vinay Patidar <vpatidar94@gmail.com>
2023-04-20 02:42:51 -0500

    fix drop event and assign teacher

--- src/Components/DragnDrop/index.js
+++ src/Components/DragnDrop/index.js
@@ -1,101 +1,90 @@
-import React, { useRef,useState } from 'react'
-import { batch, teachers_list } from '../List';
-import { time } from '../List';
+import React, { useRef, useState } from 'react';
+import { batch, teachers_list, time } from '../List';
 import "./index.css";
 
-
-const teacher_assignment = {};
-
 const DragnDrop = () => {
-    const[teacher,setTeacher]=useState("");
+  const [teacher, setTeacher] = useState("");
+  const [teacherAssignment, setTeacherAssignment] = useState({});
 
-    const dragItem = useRef();
-    const dragOverItem = useRef();
+  const dragItem = useRef();
+  const dragOverItem = useRef();
 
-    const dragStart = (e, position) => {
-        dragItem.current = position;
-        console.log(e.target.innerHTML);
-      };
-     
-      const dragEnter = (e, position) => {
-        dragOverItem.current = position;
-        console.log(e.target.innerHTML);
-      };
-     
-      const drop = (e) => {
-        const copyListItems = teachers_list;
-        const dragItemContent = copyListItems[dragItem.current];
-        // copyListItems.splice(dragItem.current, 1);
-        // copyListItems.splice(dragOverItem.current, 0, dragItemContent);
-        dragItem.current = null;
-        dragOverItem.current = null;
-        // setList(copyListItems);
-      };
+  const dragStart = (e, position) => {
+    dragItem.current = position;
+    console.log(e.target.innerHTML);
+  };
 
+  const dragEnter = (e, position, teacher) => {
+    setTeacher(teacher);
+    dragOverItem.current = position;
+    console.log(e.target.innerHTML);
+  };
 
+  const handleDrop = (e, key) => {
+    const teacher_assignment = teacherAssignment;
+    teacherAssignment[key] = teacher;
+    setTeacherAssignment({ ...teacherAssignment, teacherAssignment: teacher_assignment });
+  }
+
+  const allowDrop = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    console.log('xxx xx xx ', event.target.id);
+  }
 
   return (
     <>
-      
+
       <table className='table-style'>
-      <tbody>
-      <div className="for-time">
-      <tr>
-      <td className='F-style'>Time</td>
-        
-            
-            {batch.map((each)=>{
-                return(
-               <td className='F-style'>{each.batchname}</td> 
+        <tbody>
+          <div className="for-time">
+            <tr>
+              <td className='F-style'>Time</td>
+
+
+              {batch.map((each) => {
+                return (
+                  <td className='F-style'>{each.batchname}</td>
                 )
-            })}
-            
-        </tr>
-    </div>
-        
-        
+              })}
 
-        
-        
-    
-        <tr >
+            </tr>
+          </div>
+          <tr >
             {time.map((t) => {
-                return(
-                    
-                    <div >
-                    <td className='F-style'>{t.classtime}</td>
-                    
-                    {batch.map((b) => {
-            
-            const key = t.id + '_' + b.id
-            teacher_assignment[key] = null
-           return(
-                
-               <td className='F-style' >{teacher}</td>
-               
-               
-           )
-           })}
-            </div>
-            
-               )})}
-              
-              
+              return (
 
-        </tr>
+                <div >
+                  <td className='F-style'>{t.classtime}</td>
+
+                  {batch.map((b) => {
+
+                    const key = t.id + '_' + b.id
+                    return (
+                      <td onDragOver={allowDrop} onDrop={e => handleDrop(e, key)} key={key} id={key} className='dropzone F-style' >
+                        {teacherAssignment[key]}
+                      </td>
+                    )
+                  })}
+                </div>
+
+              )
+            })}
+
+
+
+          </tr>
         </tbody>
       </table>
 
       <div className="teacher-style">
-        {teachers_list.map((eachteacher,index)=>{
-            return(
-                <div className='each-faculty-box' draggable onDragStart={(e) => dragStart(e, index)}
-                onDragEnter={(e) => dragEnter(e, index)}
-                onDragEnd={drop}
-                key={index}>
-                <h3>{eachteacher}</h3>
-                </div>
-            )
+        {teachers_list.map((teacher, index) => {
+          return (
+            <div key={teacher} className='dropzone each-faculty-box' draggable={true} onDragStart={(e) => dragStart(e, index)}
+              onDragEnter={(e) => dragEnter(e, index, teacher)}>
+              <h3>{teacher}</h3>
+            </div>
+          )
         })}
       </div>
     </>
