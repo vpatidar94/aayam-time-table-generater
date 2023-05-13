cfab3ca5dd2486a8ae14302b7ae6461a5b0e40e9
Vinay Patidar <vpatidar94@gmail.com>
2023-05-13 08:27:31 +0530

    Added time table send option

--- web/build/static/js/14.981ce3fa.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpstarter-bt5"]=this["webpackJsonpstarter-bt5"]||[]).push([[14],{79:function(t,e,r){"use strict";r.r(e);r(1);var s=r(5);e.default=function(){return Object(s.jsx)(s.Fragment,{children:Object(s.jsx)("h1",{children:"Previous Time Table"})})}}}]);
-//# sourceMappingURL=14.981ce3fa.chunk.js.map
---
+++ web/build/static/js/17.5eb71fed.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpstarter-bt5"]=this["webpackJsonpstarter-bt5"]||[]).push([[17],{86:function(t,e,r){"use strict";r.r(e);r(1);var s=r(6);e.default=function(){return Object(s.jsx)(s.Fragment,{children:Object(s.jsx)("h1",{children:"Previous Time Table"})})}}}]);
+//# sourceMappingURL=17.5eb71fed.chunk.js.map
